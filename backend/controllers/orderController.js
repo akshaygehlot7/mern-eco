6dfe3dacba758de6e6e96014a5b5d4f1a2088f43
Akshay Gehlot <99969575+akshaygehlot7@users.noreply.github.com>
2024-05-14 11:08:26 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,5 @@
-const Order = require("../Models/orderModel");
-const Product = require("../Models/productModel");
+const Order = require("../models/orderModel");
+const Product = require("../models/productModel");
 const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
